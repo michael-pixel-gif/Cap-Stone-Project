3755a9f8b85a368b2f029cb48a61603bba0c37a9
michael-pixel-gif <m.rodriguez59313@gmail.com>
2025-05-19 15:24:16 -0700

    added images

--- data.js
+++ data.js
@@ -10,39 +10,39 @@ const entertainment = [
         link:"https://www.arizonamuseumofnaturalhistory.org/"
     },
     {
-        img:"",
+        img:"golflandsunsplash.jpg",
         name:'Golfland Sunsplash',
         rating:'⭐⭐⭐⭐+',
         pricing:'$60 under',
         type:'Amusment Park',
         location:'Mesa',
-        link:"",
+        link:"https://www.golfland.com/mesa/",
     },
     {
-        img:"",
+        img:"bulldog.jpg",
         name: 'Bulldog Canyon Off-Road Adventure Tour',
         rating:'⭐⭐⭐⭐+',
         pricing:'$100+',
         type:'nature',
         location:'Mesa',
-        link:""
+        link:"https://www.viator.com/tours/Phoenix/Bulldog-Canyon/d639-402466P1?&m=63845&supag=&supca=20927319006&supsc=&supai=&supap=&supdv=c&supnt=nt:x&suplp=9030123&supli=&supti=&tsem=true&supci=&supap1=&supap2=&synthetic_keyword=bulldog%20canyon%20adventure%20tour&gad_source=1&gad_campaignid=20933878235&gbraid=0AAAAAD-gbd7xk7zBcLNFyuYpHZLPVkAkm&gclid=CjwKCAjwravBBhBjEiwAIr30VHQHyQ63WUFOzpgg5FdC0z2OKs5-mbC1lKpTS2mE3jahaVRX70fjURoCPfUQAvD_BwE&gclsrc=aw.ds"
     },
     {
-        img:"",
+        img:"castles-n-coasters.jpg",
         name:'Castles N Coasters',
         rating:'⭐⭐⭐⭐+',
         pricing:'$60 under',
         type:'Amusment Park',
         location:'Peoria',
-        link:""
+        link:"https://www.castlesncoasters.com/"
     },
     {
-        img:"",
+        img:"carshow.jpeg",
         name:'Martin Auto Mueseum',
         rating:'⭐⭐⭐⭐+',
         pricing:'',
         type:'Mueseum',
         location:'Peoria',
-        link:""
+        link:"https://www.martinautomuseum.org/"
     }
 ]
